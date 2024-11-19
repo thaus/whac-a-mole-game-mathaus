a71b13ba0809ad6b2d77bd6927915802503fa6af
mathausr <reselane3@hotmail.com>
2024-11-18 22:55:58 -0300

    add game config constant

---
+++ src/app/features/whac-a-mole/model/whac-a-mole.constant.ts
@@ -0,0 +1,8 @@
+export const GAME_CONFIG = {
+  totalMoles: 6,
+  moleDisplayTimeMin: 1000,
+  moleDisplayTimeMax: 3000,
+  spawnIntervalMin: 500,
+  spawnIntervalMax: 1500,
+  gameDuration: 30,
+};
